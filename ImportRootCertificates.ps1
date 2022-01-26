58eab61c55fb6738a1f0182edb358fc303a3152c
Matt A <marsenault89@gmail.com>
2022-01-26 08:32:49 -0500

    Update ImportRootCertificates.ps1

--- ImportRootCertificates.ps1
+++ ImportRootCertificates.ps1
@@ -8,6 +8,5 @@ Set-Location $certdir
 # Intermediate Certificate
 Invoke-WebRequest -Uri $CertURI -OutFile rootcertificate.cer
 Import-Certificate -filepath "C:\certdownload\rootcertificate.cer" -CertStoreLocation Cert:\LocalMachine\Root
-Start-Sleep -Seconds 5
 cd C:\
 Remove-Item -Force -Recurse "$certdir"
