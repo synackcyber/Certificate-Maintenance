cbc7fe556278cbacc9f912033ca67aa08c0317c2
Matt A <marsenault89@gmail.com>
2022-01-26 08:22:02 -0500

    Update CheckCertificateSerial.ps1

--- CheckCertificateSerial.ps1
+++ CheckCertificateSerial.ps1
@@ -1,8 +1,8 @@
 # Check for serial number
-$serial = '94c95da1e850bd85209a4a2af3e1fb1604f9bb66'
-$certdir = "C:\certdownload"
+param ([string]$serial ='')
+# Vars
 $getcert = Get-ChildItem Cert:\LocalMachine\*\* | Select-String -Pattern $serial
-#$string = Out-string -InputObject $getcert
+
 if ($getcert -match $serial ) {
     Write-Host "Great! You have the certificate"
     break
