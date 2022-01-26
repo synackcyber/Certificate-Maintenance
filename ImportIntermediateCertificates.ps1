9ec8bae7933aa4c970ba8febe8b9b1c494980c8c
Matt A <marsenault89@gmail.com>
2022-01-26 08:32:37 -0500

    Update ImportIntermediateCertificates.ps1

--- ImportIntermediateCertificates.ps1
+++ ImportIntermediateCertificates.ps1
@@ -8,6 +8,5 @@ Set-Location $certdir
 # Intermediate Certificate
 Invoke-WebRequest -Uri $CertURI -OutFile intcertificate.cer
 Import-Certificate -filepath "C:\certdownload\intcertificate.cer" -CertStoreLocation Cert:\LocalMachine\CA
-Start-Sleep -Seconds 5
 cd C:\
 Remove-Item -Force -Recurse "$certdir"
